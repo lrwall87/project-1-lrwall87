74354d5854ce1d6027aa0cd26972b0e19cad97c3
lrwall87 <74141386+lrwall87@users.noreply.github.com>
2021-01-05 08:43:29 -0500

    Finished

---
+++ animal.py
@@ -0,0 +1,6 @@
+# Author: Lauren Wall
+# Date: 1/5/2021
+# Description: Asks the user for their favorite animal and then prints out that that is their favorite animal
+
+fave_animal = input("Please enter your favorite animal: ")
+print("Your favorite animal is the "+ fave_animal +".")
